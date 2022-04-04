8b87146233fa79b8b490862b8185481a91163377
josephbb <jbakcoleman@gmail.com>
2022-04-04 14:44:48 -0700

    Cleaned code and found a few bugs in Main.py

--- src/modeling.py
+++ src/modeling.py
@@ -9,63 +9,66 @@ import seaborn as sns
 from statsmodels.stats.power import tt_ind_solve_power
 import pyarrow
 
-def sample_publishing_model(df, prior={'s':.5,
-                                        't':.5,
-                                        'b':.5}):
+
+def sample_publishing_model(df,
+                            prior={'s': .5,
+                                   't': .5,
+                                   'b': .5}):
     with pm.Model() as m_1:
-        #Hyperpriors
+        # Hyperpriors
         sigma = pm.Exponential("sigma", prior['s'])
         tau = pm.Exponential("tau", prior['t'])
-        
-        bias_sigma =pm.Exponential("bias_sigma", prior['b'])
 
-        #Priors
-        noise_r = pm.Normal('noise_o', 0, sigma=sigma,shape=df.shape[0])
-        noise_o = pm.Normal('noise_r',0, sigma=sigma,shape=df.shape[0])
-        signal = pm.Normal('signal',0,tau,shape=df.shape[0])
-        bias = pm.HalfNormal('bias',bias_sigma)
+        bias_sigma = pm.Exponential("bias_sigma", prior['b'])
 
-        #Epsilon
-        s_o=1/pm.math.sqrt(df.n_o)
-        s_r=1/pm.math.sqrt(df.n_r)
+        # Priors
+        noise_r = pm.Normal('noise_o', 0, sigma=sigma, shape=df.shape[0])
+        noise_o = pm.Normal('noise_r', 0, sigma=sigma, shape=df.shape[0])
+        signal = pm.Normal('signal', 0, tau, shape=df.shape[0])
+        bias = pm.HalfNormal('bias', bias_sigma)
 
-        #Model
-        d_o = pm.Normal("d_o", mu=noise_o + signal + bias * df.direction, 
-                        sigma=s_o, observed=df.direction*df.d_o)
-        d_r = pm.Normal("d_r", mu=noise_r + signal, 
-                        sigma=s_r, observed=df.direction*df.d_r) 
+        # Epsilon
+        s_o = 1 / pm.math.sqrt(df.n_o)
+        s_r = 1 / pm.math.sqrt(df.n_r)
+
+        # Model
+        d_o = pm.Normal("d_o", mu=noise_o + signal + bias * df.direction,
+                        sigma=s_o, observed=df.direction * df.d_o)
+        d_r = pm.Normal("d_r", mu=noise_r + signal,
+                        sigma=s_r, observed=df.direction * df.d_r)
     with m_1:
-        #Prior checks and sampling
+        # Prior checks and sampling
         prior_checks = pm.sample_prior_predictive(
-            samples=50, 
+            samples=50,
             random_seed=42
         )
 
-        #Sampling
+        # Sampling
         trace_1 = pm.sample(
             cores=4,
-            target_accept=0.95, 
+            target_accept=0.95,
             random_seed=42
         )
 
-        #Posterior Predictive Checks
+        # Posterior Predictive Checks
         ppc = pm.sample_posterior_predictive(
-            trace_1, 
-            var_names=["d_o", "d_r", "signal","bias"], 
+            trace_1,
+            var_names=["d_o", "d_r", "signal", "bias"],
             random_seed=42
         )
 
-        #Convert to ArViZ
+        # Convert to ArViZ
         idata_1 = az.from_pymc3(
-            trace_1, 
-            prior=prior_checks, 
+            trace_1,
+            prior=prior_checks,
             posterior_predictive=ppc
         )
-        
-    return idata_1,trace_1, prior_checks, ppc
 
-def make_summary_table(i_data,save_loc=False):
-    table = az.summary(i_data,var_names=['sigma','tau'])
+    return idata_1, trace_1, prior_checks, ppc
+
+
+def make_summary_table(i_data, save_loc=False):
+    table = az.summary(i_data, var_names=['sigma', 'tau'])
     if save_loc:
         table.to_latex(save_loc)
     return table
